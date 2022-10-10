68cd2c93f45a4313fd72b5780087644e05740354
Harrison Grodin <hgrodin@andrew.cmu.edu>
2022-10-10 14:59:46 -0400

    Write initial simulator
    Co-authored-by: Sophia Roshal <sophia.roshal@gmail.com>

---
+++ lib/simulate.ml
@@ -0,0 +1,42 @@
+open! Core
+
+type register = Register of int
+type instruction = Add of register * register
+type registers = int list
+type alu_opcode = Add
+
+let decode : instruction -> alu_opcode * register * register = function
+  | Add (r1, r2) -> Add, r1, r2
+;;
+
+let register_fetch (registers : registers) (Register r1, Register r2) : int * int =
+  List.nth_exn registers r1, List.nth_exn registers r2
+;;
+
+let alu (alu_opcode : alu_opcode) ((i1, i2) : int * int) : int =
+  match alu_opcode with
+  | Add -> i1 + i2
+;;
+
+type program = instruction list
+
+let eval_program : program -> registers -> registers =
+  List.fold_left ~init:Fn.id ~f:(fun eval_previous instruction registers ->
+    let registers' = eval_previous registers in
+    let alu_opcode, r1, r2 = decode instruction in
+    let inputs = register_fetch registers' (r1, r2) in
+    let output = alu alu_opcode inputs in
+    output :: registers')
+;;
+
+let test () =
+  eval_program
+    [ Add (Register 0, Register 1)
+    ; Add (Register 0, Register 1)
+    ; Add (Register 0, Register 1)
+    ; Add (Register 0, Register 1)
+    ; Add (Register 0, Register 1)
+    ; Add (Register 0, Register 1)
+    ]
+    [ 1; 1 ]
+;;
