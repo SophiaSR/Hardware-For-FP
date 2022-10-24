071a5c8ffb7e79a15e787df4a44691b2a8a07d80
Harrison Grodin <hgrodin@andrew.cmu.edu>
2022-10-24 13:30:30 -0400

    Add `Const`
    Co-authored-by: Sophia Roshal <sophia.roshal@gmail.com>

--- lib/simulate.ml
+++ lib/simulate.ml
@@ -2,9 +2,11 @@ open! Core
 
 type register = Register of int
 
-type instruction =
-  | Add of register * register
-  | Sub of register * register
+type 'a instruction =
+  | Const of int
+  | Add of 'a * 'a
+  | Sub of 'a * 'a
+[@@deriving map]
 
 type registers = int list
 
@@ -12,29 +14,21 @@ type alu_opcode =
   | Add
   | Sub
 
-let decode : instruction -> alu_opcode * register * register = function
-  | Add (r1, r2) -> Add, r1, r2
-  | Sub (r1, r2) -> Sub, r1, r2
+let register_fetch (registers : registers) (Register r) : int =
+  Option.value ~default:0 (List.nth registers r)
 ;;
 
-let register_fetch (registers : registers) (Register r1, Register r2) : int * int =
-  let nth l i = Option.value ~default:0 (List.nth l i) in
-  nth registers r1, nth registers r2
+let alu : int instruction -> int = function
+  | Const i -> i
+  | Add (i1, i2) -> i1 + i2
+  | Sub (i1, i2) -> i1 - i2
 ;;
 
-let alu (alu_opcode : alu_opcode) ((i1, i2) : int * int) : int =
-  match alu_opcode with
-  | Add -> i1 + i2
-  | Sub -> i1 - i2
+let eval_instruction (instruction : register instruction) (registers : registers) : int =
+  instruction |> map_instruction (register_fetch registers) |> alu
 ;;
 
-let eval_instruction (instruction : instruction) (registers : registers) : int =
-  let alu_opcode, r1, r2 = decode instruction in
-  let inputs = register_fetch registers (r1, r2) in
-  alu alu_opcode inputs
-;;
-
-type program = instruction list
+type program = register instruction list
 
 let eval_program : program -> registers -> registers =
   List.fold_left ~init:Fn.id ~f:(fun eval_previous instruction registers ->
@@ -45,12 +39,14 @@ let eval_program : program -> registers -> registers =
 
 let test () =
   eval_program
-    [ Add (Register 0, Register 1)
+    [ Const 0
+    ; Const 1
+    ; Add (Register 0, Register 1)
     ; Add (Register 0, Register 1)
     ; Add (Register 0, Register 1)
     ; Add (Register 0, Register 1)
     ; Add (Register 0, Register 1)
     ; Add (Register 0, Register 1)
     ]
-    [ 1; 1 ]
+    []
 ;;
